1934cdf06ee5622c0372ea4ac0a20da020002bcd
yj0216 <yeojun0216@naver.com>
2022-11-21 20:20:45 +0900

    Create test code

---
+++ src/main/java/com/example/springaws/SpringAwsApplication.java
@@ -0,0 +1,14 @@
+package com.example.springaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsApplication {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springaws/web/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.springaws.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@RunWith(SpringRunner.class)//junit5부터는 자동으로 설정되서 삭제됨
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired//스프링이 관리하는 빈을 주입 받음
+    private MockMvc mvc;//스프링 테스트에 시작점
+
+    @Test
+    public void hello_return() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
